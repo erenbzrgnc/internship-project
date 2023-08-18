68a56740f1fa31e7d1f45df02a0d05b4a9695586
Eren BEZIRGANCI <eren.bezirganci@amadeus.com>
2023-08-18 10:13:23 +0300

    login form converted to reactive form

--- creating-ppc-campaign/src/app/login/login.component.ts
+++ creating-ppc-campaign/src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "../shared/services/auth.service";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -8,6 +9,20 @@ import { AuthService } from "../shared/services/auth.service";
 export class LoginComponent implements OnInit {
   constructor(
     public authService: AuthService
-  ) { }
-  ngOnInit() { }
+  ) {}
+  loginForm: FormGroup
+  ngOnInit() {
+    this.loginForm = new FormGroup({
+      "email": new FormControl(null, [Validators.required, Validators.email]),
+      "password": new FormControl(null, [Validators.required, Validators.minLength(6)])
+
+    });
+
+   }
+   onSubmit() {
+     this.authService.SignIn(this.loginForm.get("email").value, this.loginForm.get("password").value)
+    
+   }
+
+
 }
